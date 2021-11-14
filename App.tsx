b2889c02fb0a3794e9e566cc8ec4b88f7bd18395
MariusArhaug <mariusarhaug@hotmail.com>
2021-11-14 21:22:36 +0100

    CHANGE: update global styles

--- App.tsx
+++ App.tsx
@@ -5,14 +5,15 @@ import Dashboard from './app/components/Dashboard'
 import { Layout } from './app/components/Layout'
 import { apolloClient } from './app/graphql'
 import { store } from './app/store'
+import './app/styles/global.css'
 
 export default function App() {
     return (
         <Provider store={store}>
             <ApolloProvider client={apolloClient}>
-            <Layout>
-                <Dashboard />
-            </Layout>
+                <Layout>
+                    <Dashboard />
+                </Layout>
             </ApolloProvider>
         </Provider>
     )
